9098bc747dae270f7c1d487282e6ec59a9727922
Javier Ríos <202111258@alu.comillas.edu>
2023-12-02 13:40:04 +0100

    Deleted final project folder

--- Proyecto Final/prueba.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Cargar la imagen original y la imagen de entrada
-img_original = cv2.imread('Pattern/piano.png')
-img_entrada = cv2.imread('Calib_Images/image0.jpg')
-
-# quedarnos con los pizeles que esten en el rango de blancos y negros de imagen de entrada:
